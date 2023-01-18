10c990920fb6fdc92d2fad24b6c0040f26c19f80
Karan Negi <103569735+Vlankz@users.noreply.github.com>
2023-01-18 12:25:09 +0530

    added popup support

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-const moves = ['rock', 'paper', 'scissors'];
-let playerOne = "paper";
-let computer = moves[Math.floor(Math.random() * moves.length)];
+// confirmation popup 
+let playGame = confirm("Shall we play rock, paper, scissors?");
+if (playGame === true) {
+    /// play (if true)
+    let playerChoice = prompt("Choose your move");
+if (playerChoice) {
+    let playerOne = playerChoice.trim().toLocaleLowerCase(); // to trim down any whitespaces and force input to lowercase letters.
+    if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
 
-    switch (playerOne) {
+        let computerChoice = Math.floor(Math.random() * 3 + 1 ); //random number from 1 to 3
+        //if value is 1 then rock, if  then paper, scissors if 3
+        let computer = computerChoice === 1 ? "rock" 
+        : computerChoice === 2 ? "paper" 
+        : "scissors";
 
-        case computer:
-            console.log("Guess you both chose the same move, Tie Game bro");
-            break;
+        //declaring a result variable for outcome
+        let result = 
 
-        case "rock":
-            if (computer === "paper"){
-                console.log("You choose rock, Computer chose" + " " + computer + " " + "you lost bro");
-            }  else {
-                console.log("You choose rock, Computer chose" + " " + computer + " " + "you won bro");
-            } 
-            break;
+        playerOne === computer ? "tie game" 
+        : playerOne === "rock" && computer === "scissors" ? `playerOne: ${playerOne}\ncomputer: ${computer}\nPlayer wins!`
+        : playerOne === "paper" && computer === "rock" ? `playerOne: ${playerOne}\ncomputer: ${computer}\nplayer Wins!`
+        : playerOne === "scissors" && computer === "paper" ? `playerOne: ${playerOne}\ncomputer: ${computer}\nplayer Wins!`
+        :  `playerOne: ${playerOne}\ncomputer: ${computer}\computer Wins!`
 
-            case "paper":
-                if (computer === "scissors"){
-                    console.log("You choose paper, Computer chose" + " " + computer + " " + "you lost bro");
-                }  else {
-                    console.log("You choose paper, Computer chose" + " " + computer + " " + "you won bro");
-                } 
-                break;
+            alert(result);
 
-                case "scissors":
-                    if (computer === "rock"){
-                        console.log("You choose rock, Computer chose" + " " + computer + " " + "you lost bro");
-                    }  else {
-                        console.log("You choose rock, Computer chose" + " " + computer + " " + "you won bro");
-                    } 
-                    break;    
-             
-                    default: 
-                    console.log("please use a valid keyword")
+            // setting up a rematch pop up 
+            let playAgain = confirm("play again?"); 
+            playAgain ? location.reload() : alert("okay thanks for playing")
+
+    } else {
+        alert("you didnt enter any valid arguements");
     }
+
+} else {
+    alert("i guess you changed your mind.");
+}
+
+// if playGame value is false
+} else {
+    alert("okay, maybe next time.")
+}
